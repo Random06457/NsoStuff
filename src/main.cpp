66f385dbca76cc34f18f71b6ff1a93a0c2890ee3
Random <28494085+Random06457@users.noreply.github.com>
2020-11-07 00:07:33 +0100

    add command line options

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,15 @@
 
 #define ASM_DIR "out"
 
-void showUsage(std::string error)
+#define ARG_PROG    0
+#define ARG_INPUT   1
+#define ARG_MODE    2
+#define ARG_OUTPUT  3
+
+void showUsage(std::string error, char** argv)
 {
     std::cout << "ERROR: " << error << std::endl;
-    std::cout << "Usage: nsostuff file.nso" << std::endl;
+    std::cout << "Usage: " << argv[ARG_PROG] << " <input file> <mode> (<output>)" << std::endl;
     exit(1);
 }
 
@@ -19,20 +24,36 @@ int main(int argc, char** argv)
 {
     std::cout << "args:" << std::endl;
     for (size_t i = 0; i < argc; i++)
-        std::cout << "[" << i << "] \"" << argv[i] << "\"" << std::endl;
+        std::cout << "\t[" << i << "] \"" << argv[i] << "\"" << std::endl;
 
-    if (argc != 2)
-        showUsage("Invalid argument count");
+    if (argc <= 2)
+        showUsage("Too few arguments", argv);
 
-    if (!Utils::FileExists(argv[1]))
-        showUsage("Cannot open file");
+    if (!Utils::FileExists(argv[ARG_INPUT]))
+        showUsage("Input file does not exist", argv);
+    
+    std::unique_ptr<Nso> nso = std::make_unique<Nso>(argv[ARG_INPUT]);
 
-    std::unique_ptr<Nso> nso = std::make_unique<Nso>(argv[1]);
-    nso->printInfo();
-    //nso->saveDecompressed("out");
+    if (argc == 3)
+    {
+        if (!strcmp("info", argv[ARG_MODE]))
+            nso->printInfo();
+        else showUsage(std::string("Invalid mode : ") + argv[ARG_MODE], argv);
+    }
 
-    mkdir(ASM_DIR, 0777);
-    Disassembler::process(nso.get(), ASM_DIR);
+    if (argc == 4)
+    {
+        if (!strcmp(argv[ARG_MODE], "decompress"))
+        {
+            nso->saveDecompressed(argv[ARG_OUTPUT]);
+        }
+        else if (!strcmp(argv[ARG_MODE], "disassemble"))
+        {
+            mkdir(ASM_DIR, 0777);
+            Disassembler::process(nso.get(), argv[ARG_OUTPUT]);
+        }
+        else showUsage(std::string("Invalid mode : ") + argv[ARG_MODE], argv);
+    }
     
     return 0;
 }
